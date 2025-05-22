392b4df74e3efe4177dfde0331bd986cf824a3a4
sondrfos <sondre.foslien@kartverket.no>
2025-05-22 06:28:21 +0000

    style: black formatting

--- src/dask_felleskomponenter/felleskomponenter/schema.py
+++ src/dask_felleskomponenter/felleskomponenter/schema.py
@@ -1,6 +1,7 @@
 import pyspark.sql.types as ps
 import requests
 
+
 class Json2Spark:
     TypeMapping = {
         "string": ps.StringType(),
@@ -10,11 +11,18 @@ class Json2Spark:
         "integer": ps.LongType(),
         "boolean": ps.BooleanType(),
         "timestamp": ps.TimestampType(),
-        "date": ps.DateType()
+        "date": ps.DateType(),
     }
 
-    def __init__(self, json_data, enforce_required_field=True, default_type="string", defs_location="$def",
-                 circular_references=None, external_ref_base_uri=""):
+    def __init__(
+        self,
+        json_data,
+        enforce_required_field=True,
+        default_type="string",
+        defs_location="$def",
+        circular_references=None,
+        external_ref_base_uri="",
+    ):
         self.json = json_data
         self.enforce_required_field = enforce_required_field
         self.default_type = default_type
@@ -29,11 +37,18 @@ class Json2Spark:
         return True
 
     def convert2spark(self):
-        properties = self.json.get('properties', {})
+        properties = self.json.get("properties", {})
         if properties:
             fields = []
             for field_name, field_value in properties.items():
-                fields.extend(self.property2struct(field_value, field_name, f"#/properties/{field_name}", self.required_fields(self.json)))
+                fields.extend(
+                    self.property2struct(
+                        field_value,
+                        field_name,
+                        f"#/properties/{field_name}",
+                        self.required_fields(self.json),
+                    )
+                )
             return ps.StructType(fields)
         else:
             raise Exception("No properties found in JSON schema")
@@ -44,19 +59,69 @@ class Json2Spark:
         elif "$ref" in node:
             return self.refs(node["$ref"], path, field_name)
         elif "enum" in node or self.is_circular_reference(node):
-            return [ps.StructField(field_name, ps.StringType(), self.nullable(field_name, required_fields), self.metadata(path, node.get('description', '')))]
+            return [
+                ps.StructField(
+                    field_name,
+                    ps.StringType(),
+                    self.nullable(field_name, required_fields),
+                    self.metadata(path, node.get("description", "")),
+                )
+            ]
         elif "type" in node:
             if node["type"] in ["string", "number", "float", "integer", "boolean"]:
-                return [ps.StructField(field_name, self.TypeMapping.get(node["type"], ps.StringType()), self.nullable(field_name, required_fields), self.metadata(path, node.get('description', '')))]
+                return [
+                    ps.StructField(
+                        field_name,
+                        self.TypeMapping.get(node["type"], ps.StringType()),
+                        self.nullable(field_name, required_fields),
+                        self.metadata(path, node.get("description", "")),
+                    )
+                ]
             elif node["type"] == "array":
-                item_structs = self.property2struct(node.get('items', {}), "", path + "/items", self.required_fields(node.get('items', {})))
-                return [ps.StructField(field_name, ps.ArrayType(item_structs[0].dataType if item_structs else self.TypeMapping.get(self.default_type, ps.StringType())), self.nullable(field_name, required_fields), self.metadata(path, node.get('description', '')))]
+                item_structs = self.property2struct(
+                    node.get("items", {}),
+                    "",
+                    path + "/items",
+                    self.required_fields(node.get("items", {})),
+                )
+                return [
+                    ps.StructField(
+                        field_name,
+                        ps.ArrayType(
+                            item_structs[0].dataType
+                            if item_structs
+                            else self.TypeMapping.get(
+                                self.default_type, ps.StringType()
+                            )
+                        ),
+                        self.nullable(field_name, required_fields),
+                        self.metadata(path, node.get("description", "")),
+                    )
+                ]
             elif node["type"] == "object":
                 nested_fields = []
-                for k, v in node.get('properties', {}).items():
-                    nested_fields.extend(self.property2struct(v, k, f"{path}/properties/{k}", self.required_fields(node)))
-                return [ps.StructField(field_name, ps.StructType(nested_fields), self.nullable(field_name, required_fields), self.metadata(path, node.get('description', '')))]
-        return [ps.StructField(field_name, self.TypeMapping.get(self.default_type, ps.StringType()), self.nullable(field_name, required_fields), self.metadata(path, node.get('description', '')))]
+                for k, v in node.get("properties", {}).items():
+                    nested_fields.extend(
+                        self.property2struct(
+                            v, k, f"{path}/properties/{k}", self.required_fields(node)
+                        )
+                    )
+                return [
+                    ps.StructField(
+                        field_name,
+                        ps.StructType(nested_fields),
+                        self.nullable(field_name, required_fields),
+                        self.metadata(path, node.get("description", "")),
+                    )
+                ]
+        return [
+            ps.StructField(
+                field_name,
+                self.TypeMapping.get(self.default_type, ps.StringType()),
+                self.nullable(field_name, required_fields),
+                self.metadata(path, node.get("description", "")),
+            )
+        ]
 
     def required_fields(self, node):
         return node.get("required")
@@ -66,7 +131,7 @@ class Json2Spark:
         return path in self.circular_references
 
     def cursor_at(self, path):
-        parts = path.lstrip("#/").split('/')
+        parts = path.lstrip("#/").split("/")
         cursor = self.json
         for part in parts:
             cursor = cursor.get(part, {})
@@ -92,25 +157,42 @@ class Json2Spark:
     def refs(self, resource_path, base_path, field_name):
         if resource_path.startswith("#"):
             cursor = self.cursor_at(resource_path)
-            return self.property2struct(cursor, field_name, f"{base_path}/$ref/{resource_path}", self.required_fields(cursor))
+            return self.property2struct(
+                cursor,
+                field_name,
+                f"{base_path}/$ref/{resource_path}",
+                self.required_fields(cursor),
+            )
         else:
             if not resource_path.startswith("http"):
                 resource_path = self.external_ref_base_uri + "/" + resource_path
             if "#" in resource_path:
                 schema_url, fragment = resource_path.split("#", 1)
-                fragment = fragment.lstrip('/')
+                fragment = fragment.lstrip("/")
             else:
                 schema_url = resource_path
                 fragment = ""
             external_json = self.fetch_external_schema(schema_url)
             if fragment:
-                parts = fragment.split('/')
+                parts = fragment.split("/")
                 for part in parts:
                     external_json = external_json.get(part, {})
-            return self.property2struct(external_json, field_name, f"{base_path}/file/{resource_path}")
+            return self.property2struct(
+                external_json, field_name, f"{base_path}/file/{resource_path}"
+            )
 
 
-def convert_json_schema_to_spark(json_data_dict: dict, enforce_required_field=True, default_type="string", defs_location="$def") -> ps.StructType:
-    json2spark = Json2Spark(json_data=json_data_dict, enforce_required_field=enforce_required_field, default_type=default_type, defs_location=defs_location)
+def convert_json_schema_to_spark(
+    json_data_dict: dict,
+    enforce_required_field=True,
+    default_type="string",
+    defs_location="$def",
+) -> ps.StructType:
+    json2spark = Json2Spark(
+        json_data=json_data_dict,
+        enforce_required_field=enforce_required_field,
+        default_type=default_type,
+        defs_location=defs_location,
+    )
 
     return json2spark.convert2spark()
--- src/dask_felleskomponenter/governance/checks/common.py
+++ src/dask_felleskomponenter/governance/checks/common.py
@@ -7,9 +7,13 @@ import requests
 
 medaljongnivaa_kodeliste = ["bronze", "silver", "gold"]
 
+
 class CodelistUrls:
-    hovedkategori = "https://register.geonorge.no/metadata-kodelister/tematisk-hovedkategori"
+    hovedkategori = (
+        "https://register.geonorge.no/metadata-kodelister/tematisk-hovedkategori"
+    )
     begrep = "https://register.geonorge.no/metadata-kodelister/nasjonal-temainndeling"
+
 
 @dataclass
 class CodelistEntry:
@@ -22,6 +26,7 @@ class CodelistEntry:
         if not isinstance(other, CodelistEntry):
             return False
         return self.id == other.id
+
 
 @dataclass(init=False)
 class TableMetadata:
@@ -37,18 +42,19 @@ class TableMetadata:
     epsg_koder: Optional[str] = field(default=None)
     begrep: Optional[str] = field(default=None)
     sikkerhetsnivaa: Optional[str] = field(default=None)
-    
+
     column_properties: Dict[str, dict] = field(default_factory=dict)
     optional_params: Dict[str, Any] = field(default_factory=dict)
 
     def __init__(self, **kwargs):
         self.optional_params = {}
         for field_name in self.__dataclass_fields__:
-            if field_name != 'optional_params':
+            if field_name != "optional_params":
                 setattr(self, field_name, kwargs.get(field_name, None))
         for key, value in kwargs.items():
             if key not in self.__dataclass_fields__ and "delta." not in key:
                 self.optional_params[key] = value
+
 
 @dataclass
 class MetadataError:
@@ -61,36 +67,91 @@ class MetadataError:
     for_field: str
     valid_values: str | List[CodelistEntry]
 
-def get_valid_codelist_values(kodeliste_url: str, override_kodeliste_keyword: Optional[str] = None, override_kodeliste_label_keyword: Optional[str] = None) -> List[CodelistEntry]:
-    kodeliste_entry = "id" if override_kodeliste_keyword == None else override_kodeliste_keyword
-    kodeliste_label_entry = "label" if override_kodeliste_label_keyword == None else override_kodeliste_label_keyword
-    values_res = requests.get(kodeliste_url, headers={ "Accept": "application/json" }).json()
-    valid_values = list(filter(lambda x: x != None, [CodelistEntry(x.get(kodeliste_entry, None), x.get(kodeliste_label_entry, None)) for x in values_res["containeditems"]]))
+
+def get_valid_codelist_values(
+    kodeliste_url: str,
+    override_kodeliste_keyword: Optional[str] = None,
+    override_kodeliste_label_keyword: Optional[str] = None,
+) -> List[CodelistEntry]:
+    kodeliste_entry = (
+        "id" if override_kodeliste_keyword == None else override_kodeliste_keyword
+    )
+    kodeliste_label_entry = (
+        "label"
+        if override_kodeliste_label_keyword == None
+        else override_kodeliste_label_keyword
+    )
+    values_res = requests.get(
+        kodeliste_url, headers={"Accept": "application/json"}
+    ).json()
+    valid_values = list(
+        filter(
+            lambda x: x != None,
+            [
+                CodelistEntry(
+                    x.get(kodeliste_entry, None), x.get(kodeliste_label_entry, None)
+                )
+                for x in values_res["containeditems"]
+            ],
+        )
+    )
     return valid_values
 
-def get_valid_codelist_values_local(kodeliste: dict, override_kodeliste_keyword: Optional[str] = None, override_kodeliste_label_keyword: Optional[str] = None) -> List[str]:
-    kodeliste_entry = "id" if override_kodeliste_keyword == None else override_kodeliste_keyword
-    kodeliste_label_entry = "label" if override_kodeliste_label_keyword == None else override_kodeliste_label_keyword
-    
-    valid_values = list(filter(lambda x: x is not None, 
-                             [CodelistEntry(x.get(kodeliste_entry, None), x.get(kodeliste_label_entry, None)) for x in kodeliste["containeditems"]]))
-    
+
+def get_valid_codelist_values_local(
+    kodeliste: dict,
+    override_kodeliste_keyword: Optional[str] = None,
+    override_kodeliste_label_keyword: Optional[str] = None,
+) -> List[str]:
+    kodeliste_entry = (
+        "id" if override_kodeliste_keyword == None else override_kodeliste_keyword
+    )
+    kodeliste_label_entry = (
+        "label"
+        if override_kodeliste_label_keyword == None
+        else override_kodeliste_label_keyword
+    )
+
+    valid_values = list(
+        filter(
+            lambda x: x is not None,
+            [
+                CodelistEntry(
+                    x.get(kodeliste_entry, None), x.get(kodeliste_label_entry, None)
+                )
+                for x in kodeliste["containeditems"]
+            ],
+        )
+    )
+
     return valid_values
 
-def check_codelist_value(kodeliste_url: Optional[str], value: Optional[str], allowed_values: Optional[List[CodelistEntry]] = None, override_kodeliste_keyword: Optional[str] = None) -> bool:
+
+def check_codelist_value(
+    kodeliste_url: Optional[str],
+    value: Optional[str],
+    allowed_values: Optional[List[CodelistEntry]] = None,
+    override_kodeliste_keyword: Optional[str] = None,
+) -> bool:
     if value == None:
         return False
 
     if allowed_values != None:
         return value in allowed_values
-    
+
     if kodeliste_url == None:
         return value != None
 
     valid_values = get_valid_codelist_values(kodeliste_url, override_kodeliste_keyword)
     return value in valid_values
 
-def check_codelist_value_local(kodeliste_path: str, value: Optional[str], allowed_values: Optional[List[CodelistEntry]] = None, override_kodeliste_keyword: Optional[str] = None) -> bool:
+
+def check_codelist_value_local(
+    kodeliste_path: str,
+    value: Optional[str],
+    allowed_values: Optional[List[CodelistEntry]] = None,
+    override_kodeliste_keyword: Optional[str] = None,
+) -> bool:
     """
     Checks if a value exists in a local codelist
     """
@@ -99,12 +160,15 @@ def check_codelist_value_local(kodeliste_path: str, value: Optional[str], allowe
 
     if allowed_values is not None:
         return value in allowed_values
-    
+
     if kodeliste_path is None:
         return value is not None
 
-    valid_values = get_valid_codelist_values_local(kodeliste_path, override_kodeliste_keyword)
+    valid_values = get_valid_codelist_values_local(
+        kodeliste_path, override_kodeliste_keyword
+    )
     return value in valid_values
+
 
 def get_codelist(key: str) -> List[str]:
     if key == "hovedkategori":
@@ -117,10 +181,16 @@ def get_codelist(key: str) -> List[str]:
         return medaljongnivaa_kodeliste
     elif key == "sikkerhetsnivaa":
         return get_valid_codelist_values_local(sikkerhetsnivaa_kodeliste)
-    
+
     return None
+
 
 if __name__ == "__main__":
     # Example usage of both local and remote codelist checking
-    check_codelist_value("https://register.geonorge.no/api/register/sikkerhetsniva", "Ugradert", None, None)
+    check_codelist_value(
+        "https://register.geonorge.no/api/register/sikkerhetsniva",
+        "Ugradert",
+        None,
+        None,
+    )
     check_codelist_value_local("access-right.py", "PUBLIC", None, None)
